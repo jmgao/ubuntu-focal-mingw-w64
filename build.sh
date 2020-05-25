88b222e6971ae20ceba97694ddffc53085d43600
Josh Gao <josh@jmgao.dev>
2020-05-25 15:37:57 -0700

    Switch to focal.

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -euxp pipefail
 
 VERSION=$(date +%Y%m%d)
 
-docker build --no-cache -t jmgao/ubuntu-cosmic-mingw-w64:latest base
-docker tag jmgao/ubuntu-cosmic-mingw-w64:latest jmgao/ubuntu-cosmic-mingw-w64:$VERSION
+docker build --no-cache -t jmgao/ubuntu-focal-mingw-w64:latest base
+docker tag jmgao/ubuntu-focal-mingw-w64:latest jmgao/ubuntu-focal-mingw-w64:$VERSION
 
-docker build --no-cache -t jmgao/ubuntu-cosmic-mingw-w64-rust:latest rust
-docker tag jmgao/ubuntu-cosmic-mingw-w64-rust:latest jmgao/ubuntu-cosmic-mingw-w64-rust:$VERSION
+docker build --no-cache -t jmgao/ubuntu-focal-mingw-w64-rust:latest rust
+docker tag jmgao/ubuntu-focal-mingw-w64-rust:latest jmgao/ubuntu-focal-mingw-w64-rust:$VERSION
--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ set -euxp pipefail
 VERSION=$(date +%Y%m%d)
 
 git tag -a -m "$VERSION" v$VERSION
-docker push jmgao/ubuntu-cosmic-mingw-w64:latest
-docker push jmgao/ubuntu-cosmic-mingw-w64:$VERSION
+docker push jmgao/ubuntu-focal-mingw-w64:latest
+docker push jmgao/ubuntu-focal-mingw-w64:$VERSION
 
-docker push jmgao/ubuntu-cosmic-mingw-w64-rust:latest
-docker push jmgao/ubuntu-cosmic-mingw-w64-rust:$VERSION
+docker push jmgao/ubuntu-focal-mingw-w64-rust:latest
+docker push jmgao/ubuntu-focal-mingw-w64-rust:$VERSION
