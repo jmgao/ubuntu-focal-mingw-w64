26b474684b72e6813d83ba85c4996fe00f494eaa
Josh Gao <josh@jmgao.dev>
2020-05-25 15:37:11 -0700

    Disable docker build cache.

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -euxp pipefail
 
 VERSION=$(date +%Y%m%d)
 
-docker build -t jmgao/ubuntu-cosmic-mingw-w64:latest base
+docker build --no-cache -t jmgao/ubuntu-cosmic-mingw-w64:latest base
 docker tag jmgao/ubuntu-cosmic-mingw-w64:latest jmgao/ubuntu-cosmic-mingw-w64:$VERSION
 
-docker build -t jmgao/ubuntu-cosmic-mingw-w64-rust:latest rust
+docker build --no-cache -t jmgao/ubuntu-cosmic-mingw-w64-rust:latest rust
 docker tag jmgao/ubuntu-cosmic-mingw-w64-rust:latest jmgao/ubuntu-cosmic-mingw-w64-rust:$VERSION
